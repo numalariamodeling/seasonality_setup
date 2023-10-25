3800eaa33c606e00d3c6a6144f78004fe224d384
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2023-10-25 17:34:09 -0500

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,35 @@
+import os
+#
+# This is a user-modifiable Python file designed to be a set of simple input file and directory settings that you can choose and change.
+
+# the location of the file containing AssetCollection id for the dtk sif (singularity image)
+sif_id = os.path.join(os.pardir, 'dtk_sif.id')
+
+# The script is going to use this to store the downloaded schema file. Create 'download' directory or change to your preferred (existing) location.
+schema_file=os.path.join(os.path.expanduser('~'),"download/schema.json")
+
+# The script is going to use this to store the downloaded Eradication binary. Create 'download' directory or change to your preferred (existing) location.
+eradication_path=os.path.join(os.path.expanduser('~'),"download/Eradication")
+
+# Create 'Assets' directory or change to a path you prefer. idmtools will upload files found here.
+assets_input_dir="Assets"
+plugins_folder = "download/reporter_plugins"
+
+# This is where your inputs are located
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+input_dir = os.path.join(ROOT_DIR, "simulation_inputs")
+output_dir = os.path.join(ROOT_DIR, "simulation_output")
+
+
+ep4_path="python_scripts"
+#requirements = "./requirements.txt"
+
+# This is where your simulations and outputs will be stored
+job_directory = os.path.join(ROOT_DIR, 'experiments/')
+os.makedirs(job_directory, exist_ok=True)
+
+# This is the path to the sisf image used to run EMOD
+SIF_PATH = "--bind /projects /projects/b1139/images/dtk_run_rocky_py39.sif"
+
+# CSV file for building demographics
+wb_csv = "/projects/b1139/data/WB_crude_birthrate_by_year_and_country.csv"
